b0d4f5c4e956b15d2a0341984dece74673ef702f
u20241a195-cmd <u20241a195@upc.edu.pe>
2026-06-12 02:37:22 -0500

    feat(shared): add infrastructure persistence, mediator and middleware

---
+++ nextech.biotrack.platform/Shared/Infrastructure/Persistence/EntityFrameworkCore/Configuration/AppDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using nextech.biotrack.platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
+using nextech.biotrack.platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Interceptors;
+
+namespace nextech.biotrack.platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddInterceptors(new AuditableEntityInterceptor());
+        base.OnConfiguring(builder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
