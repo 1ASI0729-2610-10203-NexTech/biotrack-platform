76f6b31a3ecf738a8f4ffc61531996c99c065ccf
u20241a195-cmd <u20241a195@upc.edu.pe>
2026-06-12 02:37:15 -0500

    feat(shared): add application result and event handler interfaces

---
+++ nextech.biotrack.platform/Shared/Application/Internal/EventHandlers/IEventHandlers.cs
@@ -0,0 +1,7 @@
+﻿using nextech.biotrack.platform.Shared.Domain.Model.Events;
+using Cortex.Mediator.Notifications;
+namespace nextech.biotrack.platform.Shared.Application.Internal.EventHandlers;
+
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
+{
+}
